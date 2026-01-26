4f0d5254d164e420326b8bc6683e06e9acecfd0e
CostantiniYH <costantiniyaacov@gmail.com>
2026-01-26 23:57:49 +0100

    Affichage personnalisé + badge pseudo

--- index.php
+++ index.php
@@ -33,10 +33,8 @@ if (isset($id) && isset($_POST['select-contact'])) {
     $posts = [];
     $posts = mysqli_query($id, $get_message);
 } else {
-    echo '<div class"alert alert-warning">Veuillez sélectionner un contact pour afficher les messages</div>';
+    $e = 'Veuillez sélectionner un contact pour afficher les messages';
 }
-
-// session_destroy();
 
 ?>
 
@@ -53,48 +51,65 @@ if (isset($id) && isset($_POST['select-contact'])) {
 </head>
 <body>
     <nav class="navbar nav bg-body-tertiary bg-opacity-75 fs-4">
-        <!-- <a class="nav-link text-dark" href="register.php">Inscription</a>
-        <a class="nav-link text-black" href="login.php">Connexion</a> -->
         <a class="nav-link bi bi-door-open-fill  text-danger" href="logout.php"></a>
     </nav>
     <header class="bg-warning bg-opacity-50 mb-5 p-5 text-white">
         <h1 class="titre">Chattez avec Devsio-Chat</h1>
     </header>
     <div class="container">
-        <div class="message border-grey m-3">
-        <h3 class="text-center mb-5">Bienvenue <?= $_SESSION['pseudo'] ?></h3><hr>
+        <div class="message row border-grey ">
+            <h3 class="text-center rounded bg-body-tertiary p-5">Bienvenue <?= $_SESSION['pseudo'] ?></h3><hr>
+            <h4 class="text-center mb-5">Vos messages</h4>
 
-         <form class="form-group" method="post" class="formContent">
-            <label class="m-2" for="contact">Contacts</label><br>
-            <select class="form-control rounded-4 shadow-sm" name="contact" id="contact">
-                <option value="" class="text-gray">Sélectionner un contact</option>
-                <?php foreach ($users as $dest) { ?>
-                <option value="<?= $dest['id'] ?>"><?= $dest['pseudo'] ?></option>
-                <?php } ?>
-            </select><br>
-            <input type="submit" value="Validé" name="select-contact">
-        </form>
+            <?php if (isset($e)) { ?>
+                <div class="alert alert-warning"><?= $e ?></div>
+            <?php }  ?>
 
-            <ul class="list-group gap-3">
-                <?php 
-                if (isset($posts)) {
-                    foreach ($posts as $post) {
-                        if ($post['nom_expe'] === $_SESSION['pseudo']) {
-                            $float = "ms-auto bg-self";
-                        } else {
-                            $float = "bg-foreign";
-                        }
-                ?>
+            <div class="col-md-3 border-top mb-3">
+                <form class="form-group" method="post" class="formContent">
+                    <label class="m-2" for="contact">Contacts</label><br>
+                    <select class="form-control rounded-4 shadow-sm" name="contact" id="contact">
+                        <option value="" class="text-gray">Sélectionner un contact</option>
+                        <?php foreach ($users as $dest) { ?>
+                        <option value="<?= $dest['id'] ?>"><?= $dest['pseudo'] ?></option>
+                        <?php } ?>
+                    </select><br>
+                    <input class="btn " type="submit" value="Afficher les messages" name="select-contact">
+                </form>
+            </div>
 
-                <li class="list-group-item text-white rounded-5 w-50 <?= $float ?>">
-                    <h5><?= $post['nom_expe'] ?></h5>
-                    <p><?= $post['message'] ?></p>
-                    <small><?= $post['date'] ?></small>
-                </li>
-                <?php }
-                }; ?>
-            </ul>
-            <hr>
+            <div class="col-md border-start border-top p-1 pt-1 ps-1">
+                
+                <button type="button" class="p-3 rounded-circle bg-bleu ">
+                    <?php if (isset($posts)) foreach ($posts as $post) {
+                    if ($post['nom_expe'] !== $_SESSION['pseudo']) {
+                        $majExpe = substr($post['nom_expe'], 0, 1);
+                        echo $majExpe;
+                    }
+                    } ?>
+                </button>
+                <ul class="list-group gap-3 ps-5 pb-2">
+                    <?php 
+                    if (isset($posts)) {
+                        foreach ($posts as $post) {
+                            if ($post['nom_expe'] === $_SESSION['pseudo']) {
+                                $float = "ms-auto bg-self";
+                                $post['nom_expe'] = 'Vous';
+                            } else {
+                                $float = "bg-foreign";
+                            }
+                    ?>
+
+                    <li class="list-group-item text-white rounded-5 w-50 <?= $float ?>">
+                        <h5><?= $post['nom_expe'] ?></h5>
+                        <p><?= $post['message'] ?></p>
+                        <small><?= $post['date'] ?></small>
+                    </li>
+                    <?php }
+                    }; ?>
+                </ul>
+            </div>
+            <hr class="">
             <div class="p-5">
                 <form class="form-group" method="post" class="formContent">
                     <label class="m-2" for="destinataire">Contacts</label><br>
@@ -104,11 +119,11 @@ if (isset($id) && isset($_POST['select-contact'])) {
                         <option value="<?= $dest['id'] ?>"><?= $dest['pseudo'] ?></option>
                         <?php } ?>
                     </select><br>
-                    <!-- <label for="message">Entrer votre message</label><br> -->
-                     <div class="position-relative">
-                         <textarea class="form-control mb-3 shadow rounded-5" name="message" id="" placeholder="Message..." required></textarea>
-                         <button class="position-absolute top-50 end-0 translate-middle-y me-2 btn rounded-circle text-white bi bi-send" 
-                         type="submit" name="envoyer-message" value="Envoyer">
+
+                    <div class="position-relative">
+                        <textarea class="form-control mb-3 shadow rounded-5" name="message" id="" placeholder="Message..." required></textarea>
+                        <button class="position-absolute top-50 end-0 translate-middle-y me-2 btn rounded-circle text-white bi bi-send" 
+                        type="submit" name="envoyer-message" value="Envoyer">
                     </div>
                 </form>
             </div>
