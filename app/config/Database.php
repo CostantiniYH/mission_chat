568ba4b88632cb67b7958a9a6f95e239d3d923ac
CostantiniYH <costantiniyaacov@gmail.com>
2026-02-07 22:50:18 +0100

    migration mysqli vers pdo

--- app/config/Database.php
+++ app/config/Database.php
@@ -2,13 +2,6 @@
 namespace App\Config;
 use PDO;
 use PDOException;
-// Connexion DB
-// $id = mysqli_connect(
-//     $_ENV['DB_HOST'],
-//     $_ENV['DB_USER'],
-//     $_ENV['DB_PASS'],
-//     $_ENV['DB_NAME']
-// );
 
 class Database
 {
